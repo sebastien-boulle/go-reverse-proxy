240e24da93b94833c4bce2a42da6ef48f4fbf3ef
Sébastien Boulle <sebastien.boulle@lumapps.com>
2021-05-04 17:50:08 +0200

    feat(proxy): working implementation

--- main.go
+++ main.go
@@ -1,47 +1,117 @@
 package main
 
 import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"time"
 	"fmt"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 )
 
-func main() {
-    http.HandleFunc("/", handleRequestAndRedirect)
+const (
+        proxyPort   = 3000
+        servicePort = 3001
+        fallbackPort = 3002
+)
 
-    if err := http.ListenAndServe("127.0.0.1:3000", nil); err != nil {
-        panic(err)
-    }
-}
+type Proxy struct{}
 
-func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
-	url, err := url.Parse("http://127.0.0.1:3001")
-	if err != nil {
-		panic(err)
-	}
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// Forward the HTTP request to the destination service.
+	body, _ := ioutil.ReadAll(req.Body)
 
-	proxy := httputil.NewSingleHostReverseProxy(url)
-	proxy.ModifyResponse = catchError
-	proxy.ErrorHandler = fallback
-	proxy.ServeHTTP(res, req)
-}
-
-func catchError(res *http.Response) (err error) {
-	if res.StatusCode >= 400 {
-		return fmt.Errorf("ko")
-	}
-	return nil
-}
-
-func fallback(res http.ResponseWriter, req *http.Request, err error) {
+        res, duration, err := p.forwardRequest(req)
 	if err == nil {
-		return
+		req.Body = ioutil.NopCloser(bytes.NewReader(body))
+	        p.printStats(req, res, duration)
 	}
-	url, err := url.Parse("http://127.0.0.1:3002")
-	if err != nil {
-		panic(err)
-	}
-	proxy := httputil.NewSingleHostReverseProxy(url)
-	proxy.ServeHTTP(res, req)
+
+	// Notify the client if there was an error while forwarding the request.
+        if err != nil || res.StatusCode >= 400 {
+		req.Body = ioutil.NopCloser(bytes.NewReader(body))
+		res, duration, err = p.fallbackRequest(req)
+        }
+
+        if err != nil {
+                http.Error(w, err.Error(), http.StatusBadGateway)
+                return
+        }
+	p.printStats(req, res, duration)
+
+	// If the request was forwarded successfully, write the response back to
+	// the client.
+        p.writeResponse(w, res)
+}
+
+func main() {
+	// Listen on the predefined proxy port.
+        http.ListenAndServe(fmt.Sprintf(":%d", proxyPort), &Proxy{})
+}
+
+func (p *Proxy) forwardRequest(req *http.Request) (*http.Response, time.Duration, error) {
+	// Prepare the destination endpoint to forward the request to.
+        proxyUrl := fmt.Sprintf("http://127.0.0.1:%d%s", servicePort, req.RequestURI)
+
+	// Print the original URL and the proxied request URL.
+        fmt.Printf("Original URL: http://%s%s\n", req.Host, req.RequestURI)
+        fmt.Printf("Proxy URL: %s\n", proxyUrl)
+
+	// Create an HTTP client and a proxy request based on the original request.
+        httpClient := http.Client{}
+        proxyReq, err := http.NewRequest(req.Method, proxyUrl, req.Body)
+
+	// Capture the duration while making a request to the destination service.
+        start := time.Now()
+        res, err := httpClient.Do(proxyReq)
+        duration := time.Since(start)
+
+	// Return the response, the request duration, and the error.
+        return res, duration, err
+}
+
+func (p *Proxy) fallbackRequest(req *http.Request) (*http.Response, time.Duration, error) {
+	// Prepare the destination endpoint to forward the request to.
+        proxyUrl := fmt.Sprintf("http://127.0.0.1:%d%s", fallbackPort, req.RequestURI)
+
+	// Print the original URL and the proxied request URL.
+        fmt.Printf("Original URL: http://%s%s\n", req.Host, req.RequestURI)
+        fmt.Printf("Proxy URL: %s\n", proxyUrl)
+
+	// Create an HTTP client and a proxy request based on the original request.
+        httpClient := http.Client{}
+        proxyReq, err := http.NewRequest(req.Method, proxyUrl, req.Body)
+
+	// Capture the duration while making a request to the destination service.
+        start := time.Now()
+        res, err := httpClient.Do(proxyReq)
+        duration := time.Since(start)
+
+	// Return the response, the request duration, and the error.
+        return res, duration, err
+}
+
+func (p *Proxy) writeResponse(w http.ResponseWriter, res *http.Response) {
+	// Copy all the header values from the response.
+        for name, values := range res.Header {
+                w.Header()[name] = values
+        }
+
+        w.Header().Set("Server", "amazing-proxy")
+
+	// Set the status code returned by the destination service.
+        w.WriteHeader(res.StatusCode)
+
+	// Copy the contents from the response body.
+        io.Copy(w, res.Body)
+
+	// Finish the request.
+        res.Body.Close()
+}
+
+func (p *Proxy) printStats(req *http.Request, res *http.Response, duration time.Duration) {
+        fmt.Printf("Request Duration: %v\n", duration)
+        fmt.Printf("Request Size: %d\n", req.ContentLength)
+        fmt.Printf("Response Size: %d\n", res.ContentLength)
+        fmt.Printf("Response Status: %d\n\n", res.StatusCode)
 }
